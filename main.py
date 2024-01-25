11d12ddcf3f5af6ffc1f7b3993bf0d8a4fe36eeb
Gabriel Fernandes <gabrielsbfernandes@gmail.com>
2024-01-25 17:50:11 -0300

    I begin to explore the telegram api server and implemented a function that retrieves the data from face_description, but i need to work on the maximum length of characters

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from classes.social_net import *
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackContext
 
-manager = Social_Manager("section_n")
-print(manager.face_post_metric("467413180040037_792866542867444"))
+async def face_data(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	request_data = update.message.text.removeprefix("/request_face ")
+	date = request_data.split(' ')
+	print(f"the splited dates are {str(date)}")
+	response = Social_Manager("section_n").face_description([date[0], date[-1]])
+	message = "Algo de errado aconteceu, solicite novamente" if Social_Manager("section_n").creating_text_for_obj(response, [date[0], date[-1]]) == False else Social_Manager("section_n").creating_text_for_obj(response, [date[0], date[-1]])
+	await context.bot.send_message(chat_id=update.effective_chat.id,
+								   text=message)
 
+
+def run():
+	telegram_cred = read_config("telegram_api")
+	application = ApplicationBuilder().token(telegram_cred['token']).build()
+	req_face_handler = CommandHandler('request_face', face_data)
+	application.add_handlers([req_face_handler])
+	print("server is running now!")
+	application.run_polling()
+
+if __name__ == "__main__":
+	  run()
