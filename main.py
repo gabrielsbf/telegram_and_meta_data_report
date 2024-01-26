b009178a8ab04fab1a758ec5628eb760f0e3c4e8
Gabriel Fernandes <gabrielsbfernandes@gmail.com>
2024-01-26 17:42:35 -0300

    now the main app handles more than 4500 characters, dividing it messages

--- main.py
+++ main.py
@@ -6,10 +6,23 @@ async def face_data(update: Update, context: ContextTypes.DEFAULT_TYPE):
 	request_data = update.message.text.removeprefix("/request_face ")
 	date = request_data.split(' ')
 	print(f"the splited dates are {str(date)}")
-	response = Social_Manager("section_n").face_description([date[0], date[-1]])
-	message = "Algo de errado aconteceu, solicite novamente" if Social_Manager("section_n").creating_text_for_obj(response, [date[0], date[-1]]) == False else Social_Manager("section_n").creating_text_for_obj(response, [date[0], date[-1]])
-	await context.bot.send_message(chat_id=update.effective_chat.id,
-								   text=message)
+	response = Social_Manager("section_n").face_all_data([date[0], date[-1]])
+	separator = "------------------------------"
+	message = "Algo de errado aconteceu, solicite novamente" if Social_Manager("section_n").creating_text_for_obj(response, [date[0], date[-1]],separator) == False else Social_Manager("section_n").creating_text_for_obj(response, [date[0], date[-1]], separator)
+	limit = 4000
+	initial = 0
+	storage = message
+	print(storage)
+	while len(storage) / limit > 1:
+		subtext = storage[initial:limit]
+		len_sep = subtext.rfind(separator) + len(separator) + 1
+		text = subtext[initial:len_sep]
+		await context.bot.send_message(chat_id=update.effective_chat.id,
+									text=text)
+		storage = storage[len_sep: -1]
+	if len(storage) > 0:
+		await context.bot.send_message(chat_id=update.effective_chat.id,
+									text=storage)
 
 
 def run():
